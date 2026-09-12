1aec875f050bf175387ae72c35a672ba69261c20
mdtxx <mddevttx@gmail.com>
2026-09-12 14:40:13 +0600

    Fix formatting of cbm entry in cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -512,7 +512,7 @@ var cnames_active = {
   "cath": "cath-docs.pages.dev", // noCF
   "catisol": "catisol-ui.netlify.app",
   "cavojmidi": "cavojmidi.netlify.app",
-  "cbm": "cbmjs.github.io/cbm-website",
+  "cbm": "cbmjs.github.io/cbm-website", 
   "cdc-bot": "cdc-bot-js-npm.github.io/website",
   "cdi": "kiprox.github.io/Candro-Drive-Index",
   "cdll": "cdll.github.io",
